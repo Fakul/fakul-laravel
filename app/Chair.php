f69871bd1e390f5435551a4d114bcf9f75df779b
osvaldom <mekymaria@gmail.com>
2018-02-23 01:23:16 +0200

    create models and controlers related to courses

---
+++ app/Chair.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace fakul;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Chair extends Model
+{
+    //
+}
---
+++ app/Course.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace fakul;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    //
+}
